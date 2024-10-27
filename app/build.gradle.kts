04253b2b2c7c42921923914d90dcb49b82d5844f
Neksus87 <neksus87@gmail.com>
2024-10-27 13:11:14 +0700

    Project 2 step 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,22 @@
+import org.gradle.kotlin.dsl.dependencies as dependencies1
 plugins {
-    id("java")
+    application
+    id("checkstyle")
 }
-
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-
+application { mainClass.set("hexlet.code.App") }
 repositories {
     mavenCentral()
 }
-
-dependencies {
+dependencies1 {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
 }
