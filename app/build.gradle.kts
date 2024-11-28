edaac06fb0af6189c49ed065de73fcbd3c9a342c
Neksus87 <Neksus87@gmail.com>
2024-11-28 12:49:54 +0700

    Project 2 step 11v8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,17 +2,14 @@ plugins {
     id("application")
     id("checkstyle")
     id("com.adarshr.test-logger") version "3.2.0"
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.48.0"
-    id("jacoco")
-    kotlin("jvm") version "1.7.20" // Подключаем плагин Kotlin
+    id("io.freefair.lombok") version "8.6"
+    kotlin("jvm") version "2.0.21"
 }
 
 application {
     mainClass.set("hexlet.code.App")
-}
-
-checkstyle {
-    toolVersion = "10.3.3"
 }
 
 group = "hexlet.code"
@@ -28,49 +25,26 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.12")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
+    implementation(kotlin("stdlib-jdk8"))
 
     testImplementation("org.assertj:assertj-core:3.25.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
 
     annotationProcessor("info.picocli:picocli-codegen:4.7.5")
     implementation(kotlin("stdlib-jdk8"))
-
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.compileJava {
-    options.release = 23
-    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = "23"
-    }
-}
-
-jacoco {
-    toolVersion = "0.8.12"
-}
-
-tasks.named<JacocoReport>("jacocoTestReport") {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
-}
-
-tasks.jacocoTestReport {
-    dependsOn(tasks.test)
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
-    }
 }
 
 tasks.test {
     useJUnitPlatform()
-    finalizedBy(tasks.named("jacocoTestReport"))
+}
+
+tasks.compileJava {
+    options.release = 17
+    options.compilerArgs.add("-Aproject=${project.group}/${project.name}")
+}
+kotlin {
+    jvmToolchain(21)
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,21 +12,29 @@ import java.util.concurrent.Callable;
 class App implements Callable<Integer> {
     @Parameters(index = "0", description = "path to first file.", paramLabel = "filepath1")
     private String filepath1;
+
     @Parameters(index = "1", description = "path to second file.", paramLabel = "filepath2")
     private String filepath2;
+
     @Option(names = {"--format", "-f"}, defaultValue = "stylish",
             description = "output format [default: ${DEFAULT-VALUE}]", paramLabel = "format")
     private String format;
+
     @Override
     public Integer call() throws Exception {
 
         var diff = Differ.generate(filepath1, filepath2, format);
         System.out.println(diff);
-
-        return null;
+        return 0; // Возвращаем 0, если выполнение прошло успешно
     }
 
     public static void main(String... args) {
+        if (args.length < 2) {
+            System.err.println("Missing required parameters: '<filepath1>', '<filepath2>'");
+            System.err.println("Usage: gendiff <filepath1> <filepath2> [--format=<format>]");
+            System.exit(1);
+        }
+
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
